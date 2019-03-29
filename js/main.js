b31c75adf1c6a5d1c20eec63b8059f43ec725bca
elddiablo <elddiablo001@gmail.com>
2019-03-29 17:49:02 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    'use strict';
+
+    // onLoad -> clear fields
+    $('.contact_form--input').each(function(index, el) {
+        $(el).val('');
+    });
+
+    $('.go-down').on('click', function () {
+        $('.body, html').animate({
+            scrollTop: $('.about').offset().top
+        }, 1000)
+    });
+    $('.menu-item-box .menu-item').on('click', function (e) {
+        e.preventDefault();
+        $('.menu-item-box').removeClass('active');
+        $(this).parent('.menu-item-box').addClass('active');
+        $('html, body').animate({
+            scrollTop: $($(this).data('link')).offset().top
+        }, 1000);
+    });
+
+    $('.section-link').on('click', function (e) {
+        e.preventDefault();
+        $('.menu-item-box').removeClass('active');
+        $(this).parent('.menu-item-box').addClass('active');
+        $('html, body').animate({
+            scrollTop: $($(this).data('link')).offset().top
+        }, 1000);
+    });
+
+    $(".project-box").on('click', function() {
+        const url = $(this).data().url;
+        if(url !== "") {
+            window.open(url, '_blank');
+        }
+        
+    });
+
+    // COntact Form
+    $('#contact_form').submit(function(event) {
+        /* Act on the event */
+        event.preventDefault();
+        let inputs = {};
+        $('.contact_form--input').each(function(index, el) {
+            inputs[this.name] = $(this).val();
+        });
+
+        // console.log(inputs);
+        // $.ajax({
+        //     url: '/path/to/file',
+        //     type: 'default GET (Other values: POST)',
+        //     dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+        //     data: {param1: 'value1'},
+        // })
+        // .done(function() {
+        //     console.log("success");
+        // })
+        // .fail(function() {
+        //     console.log("error");
+        // })
+        // .always(function() {
+        //     console.log("complete");
+        // });
+        
+
+        setTimeout(() => {
+            $('#to_hide').fadeOut('slow', function() {
+                alert("Thank you for reaching me out, I will reach you out as soon as possible.");
+                $('.contact_form--input').each(function(index, el) {
+                    $(el).val('');
+                });
+            });
+        }, 1000);
+
+        
+    });
+
+    if ($('.projects-slider').length) {
+        $('.projects-slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            dots: true,
+            centerMode: true,
+            focusOnSelect: true,
+            responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 1
+                }
+            }]
+        });
+    }
+    
+});
