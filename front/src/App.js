736a84ac77dcda376750ae09476f196ca0fbb815
nick_parovoy <32142855+nickparov@users.noreply.github.com>
2020-06-27 21:49:32 -0500

    + main banner + list basic view

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,111 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
+import { Parallax, Background } from 'react-parallax';
+
+import Typist from 'react-typist';
+
+
+import parallaxBackground from './assets/images/parallaxBackground.jpg';
+
+import { 
+  Jumbotron, 
+  Button ,
+  Container,
+  Row,
+  Col,
+  Figure
+} from 'react-bootstrap';
+
+
+const COLORS = {
+  dark: "rgba(44, 44, 46, 0.9)",
+  light: "rgba(229, 229, 234, 0.9)"
+}
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Parallax
+            blur={5}
+            bgImage={parallaxBackground}
+            bgImageAlt="the cat"
+            strength={750}
+            style={{paddingTop: "16px"}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container className={"d-flex align-itmes-center justify-content-center flex-column"} style={{height: window.innerHeight}}>
+              <Jumbotron className={"text-center theme-dark"}>
+                <h1><Typist>Welcome to my Portfolio!</Typist></h1>
+                <p><Typist startDelay={1600}>Scroll down to see my projects!</Typist></p>
+              </Jumbotron>
+          </Container>
+
+          <Jumbotron className={"theme-dark"}>
+              <h1 className={"text-center mb-4"} >
+                My Projects
+              </h1>
+              <Container>
+                <Row>
+                  <Col md="4">
+                      <Figure style={{margin: "0px", position: "relative"}}>
+                        <Figure.Image
+                          style={{margin: "0px"}}
+                          fluid
+                          rounded
+                          alt="171x180"
+                          src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
+                        />
+                        <div className="figure-cover">
+                          <p className={"text-center"}>Course Tracker</p>
+                        </div>
+                      </Figure>
+                  </Col>
+                  <Col md="4">
+                      <Figure style={{margin: "0px", position: "relative"}}>
+                        <Figure.Image
+                          style={{margin: "0px"}}
+                          fluid
+                          rounded
+                          alt="171x180"
+                          src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
+                        />
+                        <div className="figure-cover">
+                          <p className={"text-center"}>Course Tracker</p>
+                        </div>
+                      </Figure>
+                  </Col>
+                  <Col md="4">
+                      <Figure style={{margin: "0px", position: "relative"}}>
+                        <Figure.Image
+                          style={{margin: "0px"}}
+                          fluid
+                          rounded
+                          alt="171x180"
+                          src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
+                        />
+                        <div className="figure-cover">
+                          <p className={"text-center"}>Course Tracker</p>
+                        </div>
+                      </Figure>
+                  </Col>
+                </Row>
+            </Container>
+          </Jumbotron>
+        </Parallax>
+      
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+/* <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron> */
--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// bootstrap styles
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
